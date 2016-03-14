e006b579da34e1d2343cc8eea731009b5eef5581
Yogie Putra <yogie.putra8@gmail.com>
2016-03-15 01:13:37 +0700

    Add validator in todo

--- models/todo.js
+++ models/todo.js
@@ -1,16 +1,41 @@
+// module.exports = function(sequelize, DataTypes) {
+//   return sequelize.define('todo', {
+//     description: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//       validate: {
+//         len: [1, 250]
+//       }
+//     },
+//     completed: {
+//       type: DataTypes.BOOLEAN,
+//       allowNull: false,
+//       defaultValue: false
+//     }
+//   });
+// };
+
+
 module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('todo', {
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1, 250]
-      }
-    },
-    completed: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    }
-  });
+	return sequelize.define('todo', {
+		description: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [1, 250]
+			}
+		},
+		completed: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			defaultValue: false,
+			validate: {
+				isBoolean: function (value) {
+					if (typeof value !== 'boolean') {
+						throw new Error('Completed must be a boolean');
+					}
+				}
+			}
+		}
+	});
 };
