4e8ec9e5f1f3aad0fd801ed6e8eb7bb36a09c814
Yogie Putra <yogie.putra8@gmail.com>
2016-03-14 23:53:57 +0700

    Convert DELETE /todos/:id to sequelize

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.post('/todos', function(req, res) {
   }, function(e){
     res.status(400).json(e);
   });
- 
+
 });
 
 
@@ -75,18 +75,35 @@ app.post('/todos', function(req, res) {
 // DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
   var todoId = parseInt(req.params.id, 10);
-  var matchedTodo = _.findWhere(todos, {
-    id: todoId
+
+  db.todo.destroy({
+    where: {
+      id: todoId
+    }
+  }).then(function(rowsDeleted) {
+    if(rowsDeleted === 0){
+      res.status(404).json({
+        error: 'No todo with id'
+      });
+    } else {
+      res.status(204).send();
+    }
+  }, function (){
+    res.status(500).send();
   });
 
-  if (!matchedTodo) {
-    res.status(404).json({
-      "errror": "no todo found with that ID!"
-    })
-  } else {
-    todos = _.without(todos, matchedTodo);
-    res.json(matchedTodo);
-  }
+  // var matchedTodo = _.findWhere(todos, {
+  //   id: todoId
+  // });
+  //
+  // if (!matchedTodo) {
+  //   res.status(404).json({
+  //     "errror": "no todo found with that ID!"
+  //   })
+  // } else {
+  //   todos = _.without(todos, matchedTodo);
+  //   res.json(matchedTodo);
+  // }
 
 });
 
